dc36a1f5dada9f095ae16387cf3ee2d7a128abf0
Leonardo Neves <leonardo.neves@tecnico.ulisboa.pt>
2026-05-15 23:51:04 +0100

    add problems 6-7

--- p5.lisp
+++ p5.lisp
@@ -1,21 +1,6 @@
-(defun is-palindrome (lst)
-    (if (null lst) ; terminal case where list is empty
-        t
-        (if (null (cdr lst)) ; terminal case where list has 1 element
-            t
-            (if (equal (car lst) (car (last lst))) ; normal case -> compares 1st with last element
-                (is-palindrome (cdr (butlast lst))) ; recursive call with list except 1st and last element
-                nil))))
-
-(print (is-palindrome '(a b c d e d c b a)))
-
-;; OR -> solution using cond
-
-(defun is-palindrome-v2 (lst)
+(defun reverse-lst (lst)
     (cond 
-        ((null lst) t)
-        ((null (cdr lst)) t)
-        ((equal (car lst) (car (last lst))) (is-palindrome-v2 (cdr (butlast lst))))
-        (t nil)))
+        ((null lst) ())
+        (t (append (last lst) (reverse-lst (butlast lst))))))
 
-(print (is-palindrome-v2 '(a b c e e e c b a)))
+(print (reverse-lst '(a b c d e f g h i)))
---
+++ p7.lisp
@@ -0,0 +1,7 @@
+(defun normalize (lista12)
+    (cond
+        ((atom lista12) (list lista12))
+        ((null (cdr lista12)) (normalize (car lista12)))
+        (t (append (normalize (car lista12)) (normalize (cdr lista12))))))
+
+(print (normalize '(a (b (c d) e))))
